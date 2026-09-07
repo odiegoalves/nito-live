12a4db77eed69b2219a546dbfc8bb8316aa4f3bb
odiegoalves <diegoalvesdealmeida14@gmail.com>
2026-09-07 13:49:43 -0300

    Add 'x-client-info' and 'apikey' to CORS headers

--- supabase/functions/chaves/index.ts
+++ supabase/functions/chaves/index.ts
@@ -23,7 +23,7 @@ import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
 const CORS = {
   "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Headers": "authorization, content-type",
+  "Access-Control-Allow-Headers": "authorization, content-type, x-client-info, apikey",
   "Access-Control-Allow-Methods": "POST, OPTIONS",
 };
 
